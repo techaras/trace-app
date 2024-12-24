50997ba9361ae47acff4040b84f8eb67a27acf05
Stavros Symeonidis <stavrossymeonidis95@gmail.com>
2024-12-24 11:59:58 +0000

    1st attempt to fix bad gateway error

--- app.py
+++ app.py
@@ -178,5 +178,6 @@ def home():
     return "Flask server is running!"
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
 
